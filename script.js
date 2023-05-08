b31db922b704dc8358afe7a773f73dcd0c0643ac
melias08 <meliasanaya@uniguajira.edu.co>
2023-05-08 16:32:19 -0500

    Se agrega el Proyecto

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarLista() {
+    var lista = document.getElementById("miLista");
+    if (lista.style.display === "none") {
+      lista.style.display = "block";
+    } else {
+      lista.style.display = "none";
+    }
+  }
+  
